b913d35f45fbf8dd4e4755865994458b9c0854a5
Meet Kumar Jain <meetkjain83@gmail.com>
2023-06-03 08:34:46 +0530

    changes

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -48,8 +48,8 @@ flexelementnode1a.setAttribute('href', 'https://www.apollotyres.com/en-in/car-su
 flexelementnode1.appendChild(flexelementnode1a);
 
 var flexelementnode1p=document.createElement('p');
-flexelementnode1p.setAttribute('class', 'vehicle-category-label1');
-var flextextnode1p=document.createTextNode('Car1, SUV1 & Van1');
+flexelementnode1p.setAttribute('class', 'vehicle-category-label');
+var flextextnode1p=document.createTextNode('CAR, SUV & VAN');
 flexelementnode1p.appendChild(flextextnode1p);
 flexelementnode1a.appendChild(flexelementnode1p);
 
@@ -65,12 +65,12 @@ elementnode3.appendChild(flexelementnode2);
 
 var flexelementnode2a=document.createElement('a');
 flexelementnode2a.setAttribute('class', 'vehicle-category');
-flexelementnode2a.setAttribute('href', 'https://www.apollotyres.com/en-in/car-suv-tyres/');
-flexelementnode1.appendChild(flexelementnode2a);
+flexelementnode2a.setAttribute('href', 'https://www.apollotyres.com/en-in/bike-scooter-tyres/');
+flexelementnode2.appendChild(flexelementnode2a);
 
 var flexelementnode2p=document.createElement('p');
-flexelementnode2p.setAttribute('class', 'vehicle-category-label2');
-var flextextnode2p=document.createTextNode('Car2, SUV2 & Van2');
+flexelementnode2p.setAttribute('class', 'vehicle-category-label');
+var flextextnode2p=document.createTextNode('BIKE & SCOOTER');
 flexelementnode2p.appendChild(flextextnode2p);
 flexelementnode2a.appendChild(flexelementnode2p);
 
@@ -78,3 +78,85 @@ flexelementnode2a.appendChild(flexelementnode2p);
 
 
 
+
+
+var flexelementnode3=document.createElement('div');
+var flexattributenode3=document.createAttribute('class');
+flexattributenode3.value="item";
+flexelementnode3.setAttributeNode(flexattributenode3);
+elementnode3.appendChild(flexelementnode3);
+
+var flexelementnode3a=document.createElement('a');
+flexelementnode3a.setAttribute('class', 'vehicle-category');
+flexelementnode3a.setAttribute('href', 'https://www.apollotyres.com/en-in/agricultural-tyres/');
+flexelementnode3.appendChild(flexelementnode3a);
+
+var flexelementnode3p=document.createElement('p');
+flexelementnode3p.setAttribute('class', 'vehicle-category-label');
+var flextextnode3p=document.createTextNode('AGRICULTURAL');
+flexelementnode3p.appendChild(flextextnode3p);
+flexelementnode3a.appendChild(flexelementnode3p);
+
+
+
+
+
+var flexelementnode4=document.createElement('div');
+var flexattributenode4=document.createAttribute('class');
+flexattributenode4.value="item";
+flexelementnode4.setAttributeNode(flexattributenode4);
+elementnode3.appendChild(flexelementnode4);
+
+var flexelementnode4a=document.createElement('a');
+flexelementnode4a.setAttribute('class', 'vehicle-category');
+flexelementnode4a.setAttribute('href', 'https://www.apollotyres.com/en-in/bike-scooter-tyres/');
+flexelementnode4.appendChild(flexelementnode4a);
+
+var flexelementnode4p=document.createElement('p');
+flexelementnode4p.setAttribute('class', 'vehicle-category-label');
+var flextextnode4p=document.createTextNode('TRUCK & BUS');
+flexelementnode4p.appendChild(flextextnode4p);
+flexelementnode4a.appendChild(flexelementnode4p);
+
+
+
+
+var flexelementnode5=document.createElement('div');
+var flexattributenode5=document.createAttribute('class');
+flexattributenode5.value="item";
+flexelementnode5.setAttributeNode(flexattributenode5);
+elementnode3.appendChild(flexelementnode5);
+
+var flexelementnode5a=document.createElement('a');
+flexelementnode5a.setAttribute('class', 'vehicle-category');
+flexelementnode5a.setAttribute('href', 'https://www.apollotyres.com/en-in/industrial-tyres/');
+flexelementnode5.appendChild(flexelementnode5a);
+
+var flexelementnode5p=document.createElement('p');
+flexelementnode5p.setAttribute('class', 'vehicle-category-label');
+var flextextnode5p=document.createTextNode('INDUSTRIAL');
+flexelementnode5p.appendChild(flextextnode5p);
+flexelementnode5a.appendChild(flexelementnode5p);
+
+
+
+
+
+
+
+var flexelementnode6=document.createElement('div');
+var flexattributenode6=document.createAttribute('class');
+flexattributenode6.value="item";
+flexelementnode6.setAttributeNode(flexattributenode6);
+elementnode3.appendChild(flexelementnode6);
+
+var flexelementnode6a=document.createElement('a');
+flexelementnode6a.setAttribute('class', 'vehicle-category');
+flexelementnode6a.setAttribute('href', 'https://www.apollotyres.com/en-in/earthmover-tyres/');
+flexelementnode6.appendChild(flexelementnode6a);
+
+var flexelementnode6p=document.createElement('p');
+flexelementnode6p.setAttribute('class', 'vehicle-category-label');
+var flextextnode6p=document.createTextNode('EARTHMOVER');
+flexelementnode6p.appendChild(flextextnode6p);
+flexelementnode6a.appendChild(flexelementnode6p);
